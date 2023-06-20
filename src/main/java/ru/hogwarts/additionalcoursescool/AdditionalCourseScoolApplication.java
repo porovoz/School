e44bdb46cffe16f17bd1f8d6e500f290e3be1ed6
porovoz <falkon1@list.ru>
2023-06-20 16:15:14 +0300

    initial commit

---
+++ src/main/java/ru/hogwarts/additionalcoursescool/AdditionalCourseScoolApplication.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.additionalcoursescool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdditionalCourseScoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdditionalCourseScoolApplication.class, args);
+    }
+
+}
